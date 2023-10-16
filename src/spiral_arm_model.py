2ffcbb9816769cf576f5665ef8733001a89d5464
Viktor <viktor.mikalsen@gmail.com>
2023-10-16 16:41:44 +0200

    Calculated effective area, density of all arms plotted

--- src/spiral_arm_model.py
+++ src/spiral_arm_model.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+from scipy.interpolate import interpn
+import seaborn as sns
+
 
 # constants
 h = 2.5                 # kpc, scale length of the disk. The value Higdon and Lingenfelter used
@@ -17,6 +21,7 @@ a_d = 2*np.pi*h**2 * ((1+rho_min/h)*np.exp(-rho_min/h) - (1+rho_max/h)*np.exp(-r
 arm_angles = np.radians([70, 160, 250, 340])
 # pitch angles for the spiral arms, respectively Norma-Cygnus, Perseus, Sagittarius-Carina, Scutum-Crux
 pitch_angles = np.radians(np.array([13.5, 13.5, 13.5, 15.5]))
+number_of_end_points = 181 # number of points to use for the circular projection at the end points of the spiral arms
 
 
 def spiral_arm_medians(arm_angle, pitch_angle):
@@ -50,7 +55,7 @@ def height_distribution(z): # z is the height above the Galactic plane
     return np.exp(-0.5 * z**2 / sigma**2) / (np.sqrt(2*np.pi) * sigma)
 
 
-def transverse_arm_distribution(delta, sigma_arm):
+def arm_transverse_density(delta, sigma_arm):
     """
     Args:
         delta: transverse distance from the medians of the modelled spiral arm
@@ -61,17 +66,15 @@ def transverse_arm_distribution(delta, sigma_arm):
     return np.exp(-0.5 * delta**2 / sigma_arm**2)  / (np.sqrt(2*np.pi) * sigma_arm) # in the paper, they do not include this normalization factor for some reason
 
 
-def rotate_point(point, angle, rotation_point=(0, 0)):
+def arm_median_density(rho): 
     """
     Args:
-        point: point or points to rotate. (x,y) coordinates, (n, 2) matrix with n = number of points
-        angle: angle to rotate the point by. Radians
-        rotation_point: the point to rotate around. (x,y) coordinates
+        rho: distance from the Galactic center
+        h: scale length of the disk
     Returns:
-        rotated point
+        density of the disk at a distance rho from the Galactic center
     """
-    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-    return np.matmul(point - rotation_point, rotation_matrix.T) + rotation_point
+    return np.exp(-rho/h)
 
 
 def generate_non_uniform_spacing(sigma_arm = 0.5, d_min = 0.01, d_max = 5, scaling = 0.03):
@@ -81,8 +84,7 @@ def generate_non_uniform_spacing(sigma_arm = 0.5, d_min = 0.01, d_max = 5, scali
         d_max: maximum distance from the spiral arm
         scaling: scaling of the exponential distribution. A value of 0.03 generates between 150-200 points, for the most part 160-180 points
     Returns:
-        an array of incremental distances for rho. The sum of all elements in the array is equal to 4.99 (d_max - d_min).
-        To get a non-uniform spacing of points, an exponential distribution is used.
+        1D array with the relative distances for the transverse parts of the spiral arms in radial units rho. First element is d_min, last element is d_max
     """
     d_rho = np.array([d_min])
     while np.sum(d_rho) < d_max: # to make sure that the dirst dx shall be d_min
@@ -93,7 +95,8 @@ def generate_non_uniform_spacing(sigma_arm = 0.5, d_min = 0.01, d_max = 5, scali
     d_rho[-1] = d_rho[-1] - diff
     d_rho.sort()
     transverse_distances = np.cumsum(d_rho)
-    transverse_densities = transverse_arm_distribution(transverse_distances, sigma_arm)
+    print(transverse_distances[-1])
+    transverse_densities = arm_transverse_density(transverse_distances, sigma_arm)
     return transverse_distances, transverse_densities
 
 
@@ -131,22 +134,23 @@ def generate_end_points(rho, theta, pitch_angle, transverse_distances, point='st
         rho: radial distance to one of the end points of the spiral arm
         theta: angular distance to one of the end points of the spiral arm
     Returns:
-        a 
+        a 3D array with the xy coordinates for the half-sphere around the end-point of the spiral arm
+        axis = 0 is basically an index for the angle of the circular projection
+        axis = 1 is an index for the points on the circular projection
+        axis = 2 is an index for the x and y coordinates
     """
-    angles_arc = np.linspace(0, np.pi, num=180) + theta - pitch_angle 
+    angles_arc = np.linspace(0, np.pi, num=number_of_end_points) + theta - pitch_angle 
     if point == 'start':
         angles_arc += np.pi
     x_arc = rho * np.cos(theta) + transverse_distances * np.cos(angles_arc)[:, np.newaxis]
     y_arc = rho * np.sin(theta) + transverse_distances * np.sin(angles_arc)[:, np.newaxis]
-    #x = np.cos(thetas)[:, np.newaxis] * rhos
-    #y = np.sin(thetas)[:, np.newaxis] * rhos
-    return np.array([x_arc, y_arc]).T
+    return np.concatenate((x_arc[:, :, np.newaxis], y_arc[:, :, np.newaxis]), axis=2)
 
 
 
 def plot_spiral_arms():
     """
-    Plots the spiral arms
+    Plots the spiral arms, both the medians and also the transverse points. 
     """
     plt.scatter(0,0, c='magenta')
     colours = ['navy', 'darkgreen', 'darkorange', 'purple'] # one colour per arm
@@ -158,7 +162,7 @@ def plot_spiral_arms():
         y = rho*np.sin(theta)
         # generate the transverse points
         transverse_points = generate_transverse_points(rho, transverse_distances, theta, pitch_angles[i])
-        # Flatten the 3D array into 2D arrays
+        # Flatten the 3D array into 2 1D arrays
         x_coords = transverse_points[:, :, 0].flatten()
         y_coords = transverse_points[:, :, 1].flatten()
         # generate the end points
@@ -173,4 +177,168 @@ def plot_spiral_arms():
     plt.savefig("output/spiral_arms_2_w_transverse_contribution.png")  # save plot in the output folder
     plt.show()
 
-plot_spiral_arms()
+
+def generate_spiral_arm_points_spherical_coords(rho, theta, pitch_angle, transverse_distances):
+    """
+    Args:
+        rho: 1D array of radial distances to the spiral arm median
+        theta: 1D array of angular distances to the spiral arm median
+        pitch_angle: pitch angle of the spiral arm
+        transverse_distances: 1D array of transverse distances from the spiral arm median
+    Returns:
+        rho_coords, theta_coords
+        for a given spiral arm, the spherical coordinates for the spiral arm are returned. Includes arm median, the transverse points and the circular projection at the end points
+        rho_coords, theta_coords are 1D arrays
+    """
+    # generate the transverse points. This is a 3D array: the first index is the point on the spiral arm, the second index is the transverse point, and the third index is the x and y coordinates
+    transverse_points = generate_transverse_points(rho, transverse_distances, theta, pitch_angle)
+    # convert the transverse points to spherical coordinates, and insert the arm medians rho and theta in the middle of the arrays
+    pos_arm_median = len(transverse_points[0]) // 2
+    radial_transverse_points = np.sqrt(transverse_points[:, :, 0]**2 + transverse_points[:, :, 1]**2)
+    radial_transverse_points = np.insert(radial_transverse_points, pos_arm_median, rho, axis=1)
+    angular_transverse_points = np.arctan2(transverse_points[:, :, 1], transverse_points[:, :, 0])
+    angular_transverse_points = np.insert(angular_transverse_points, pos_arm_median, theta, axis=1)
+    # add them together to get the spherical coordinates in a 3D array. Axis 0 is the point along the spiral arm median, axis 1 is the transverse point, axis 2 is the rho and theta coordinates
+    spherical_transverse_points = np.concatenate((radial_transverse_points[:, :, np.newaxis], angular_transverse_points[:, :, np.newaxis]), axis=2)
+    # generate the end points for the spiral arm. 3D array, axis 0 is basically the angle for the circular projection, axis 1 are the points, axis 2 are the x and y coordinates
+    start_arm = generate_end_points(rho[0], theta[0], pitch_angle, transverse_distances, 'start')
+    end_arm = generate_end_points(rho[-1], theta[-1], pitch_angle, transverse_distances, 'end')
+    # get the endpoints in spherical coordinates
+    radial_start_arm = np.sqrt(start_arm[:, :, 0]**2 + start_arm[:, :, 1]**2)
+    angular_start_arm = np.arctan2(start_arm[:, :, 1], start_arm[:, :, 0])
+    radial_end_arm = np.sqrt(end_arm[:, :, 0]**2 + end_arm[:, :, 1]**2)
+    angular_end_arm = np.arctan2(end_arm[:, :, 1], end_arm[:, :, 0])
+    # now we need to gather the arm and endpoints together
+    rho_coords = np.append(radial_start_arm.flatten(), radial_transverse_points.flatten())
+    rho_coords = np.append(rho_coords, radial_end_arm.flatten())
+    theta_coords = np.append(angular_start_arm.flatten(), angular_transverse_points.flatten())
+    theta_coords = np.append(theta_coords, angular_end_arm.flatten())
+    return rho_coords, theta_coords
+
+
+def generate_spiral_arm_densities(rho, transverse_densities_initial):
+    """
+    Args:
+        rho: 1D array of radial distances to the spiral arm median
+        transverse_densities_initial: 1D array of transverse distances from the spiral arm median. Initial simply indicates that these are the distances on one side of the spiral arm median
+    Returns:
+        density_spiral_arm: 1D array of densities for the spiral arm. The densities are calculated for the spiral arm median, the transverse points and the circular projection at the end points
+        the densities appears in the same order as the spiral arm points in rho_coords and theta_coords as returned by generate_spiral_arm_points_spherical_coords
+    """
+    transverse_densities = np.append(1, transverse_densities_initial) # the 1 is to take into account the arm median itself
+    transverse_densities = np.append(np.flip(transverse_densities_initial), transverse_densities) # so that we have the transverse densities on both sides of the spiral arm
+    # calculate the densities for the arm median
+    arm_median_densities = arm_median_density(rho) #1D array
+    # calculate the transverse densities for the arm. Does not contain the contrbution from the circular projection at the end points, but has the arm median density
+    arm_transverse_densities = transverse_densities * arm_median_densities[:, np.newaxis] #2D array
+    # calculate the densities for the end points projected in a circle around the end points
+    density_start_arm = transverse_densities_initial * arm_median_densities[0] # this is a 1D array, but the same values goes for every index in start_arm along axis 0
+    density_end_arm = transverse_densities_initial * arm_median_densities[-1] # this is a 1D array, but the same values goes for every index in end_arm along axis 0
+    density_spiral_arm = np.concatenate([np.tile(density_start_arm, number_of_end_points), arm_transverse_densities.flatten(), np.tile(density_end_arm, number_of_end_points)])
+    return density_spiral_arm
+    
+
+def plot_model_spiral_arm_densities():
+    """
+    Returns:
+        a plot of the modelled spiral arm densities. Each arm is plotted in a separate subplot as a heatmap to indicate the density
+    """
+    transverse_distances, transverse_densities_initial = generate_non_uniform_spacing(d_min=0.01)
+    # Define the number of rows and columns in the subplot grid
+    num_rows, num_cols = 2, 2
+    # Create subplots for each arm
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(10, 10))
+    # Adjust the horizontal and vertical spacing
+    plt.subplots_adjust(hspace=0.01, wspace=0.3)
+    for i in range(num_rows):
+        for j in range(num_cols):
+            ax = axes[i, j]
+            # generate the spiral arm medians
+            theta, rho = spiral_arm_medians(arm_angles[i * num_cols + j], pitch_angles[i * num_cols + j])
+            # generate the spiral arm points in spherical coordinates
+            rho_coords, theta_coords = generate_spiral_arm_points_spherical_coords(rho, theta, pitch_angles[i * num_cols + j], transverse_distances)
+            density_spiral_arm = generate_spiral_arm_densities(rho, transverse_densities_initial)
+            # Convert to Cartesian coordinates and plot the scatter plot
+            x = rho_coords * np.cos(theta_coords)
+            y = rho_coords * np.sin(theta_coords)
+            ax.set_xlim(-40, 40)
+            ax.set_ylim(-40, 40)
+            ax.set_aspect('equal', adjustable='box')
+            ax.scatter(x, y, c=density_spiral_arm, s=20)
+            ax.set_xlabel('Distance in kpc from the Galactic center')
+            ax.set_ylabel('Distance in kpc from the Galactic center')
+            ax.set_title(f'Spiral Arm {i * num_cols + j + 1}')
+    # Add a colorbar
+    cbar = fig.colorbar(ax.collections[0], ax=axes, orientation='vertical')
+    cbar.set_label('Density')
+    plt.suptitle('Heatmap of the densities of spiral arms in our model')
+    plt.savefig("output/spiral_arms_density_model.png", dpi=300)
+    #plt.show()  # To display the plot
+
+
+def plot_interpolated_galactic_densities():
+    """
+    Returns:
+        a plot of the interpolated galactic densities. The galactic densities are interpolated from the individual spiral arm densities
+        the plot is a heatmap to indicate the density, and all spiral arms are plotted in the same plot
+    """
+    transverse_distances, transverse_densities_initial = generate_non_uniform_spacing(d_min=0.01) #d_min: minimum distance from the spiral arm
+    grid_x, grid_y = np.mgrid[-35:35:1000j, -35:35:1000j]
+    total_galactic_density = np.zeros((1000, 1000))
+    for i in range(len(arm_angles)):
+        # generate the spiral arm medians
+        theta, rho = spiral_arm_medians(arm_angles[i], pitch_angles[i])
+        # generate the spiral arm points in spherical coordinates
+        rho_coords, theta_coords = generate_spiral_arm_points_spherical_coords(rho, theta, pitch_angles[i], transverse_distances)
+        # generate the spiral arm densities
+        density_spiral_arm = generate_spiral_arm_densities(rho, transverse_densities_initial)
+        # Convert to Cartesian coordinates
+        x = rho_coords*np.cos(theta_coords)
+        y = rho_coords*np.sin(theta_coords)
+        # calculate interpolated density for the spiral arm
+        interpolated_density = griddata((x, y), density_spiral_arm, (grid_x, grid_y), method='linear', fill_value=0)
+        # add the interpolated density to the total galactic density
+        total_galactic_density += interpolated_density
+    #plot heatmap of the interpolated densities:
+    plt.scatter(grid_x, grid_y, c=total_galactic_density.flatten(), cmap='viridis', s=20)
+    plt.gca().set_aspect('equal')
+    plt.xlabel('Distance in kpc from the Galactic center')
+    plt.ylabel('Distance in kpc from the Galactic center')
+    plt.title('Heatmap of the interpolated densities of spiral arms in our model')
+    cbar = plt.colorbar()
+    cbar.set_label('Density')
+    plt.savefig("output/interpolated_spiral_arms_density_model.png", dpi=600)
+
+
+def calc_effective_area_per_spiral_arm():
+    """
+    Calculates the effective area for each spiral arm. The density of each spiral arm is integrated over the entire galactic plane.
+    The returned effective areas are in units of kpc^2, and appears in the same order as the spiral arms in arm_angles.
+    """
+    transverse_distances, transverse_densities_initial = generate_non_uniform_spacing(d_min=0.01) #d_min: minimum distance from the spiral arm
+    d_x = 70 / 1000 # distance between each interpolated point in the x direction. 70 kpc is the diameter of the Milky Way, 1000 is the number of points
+    d_y = 70 / 1000 # distance between each interpolated point in the y direction. 70 kpc is the diameter of the Milky Way, 1000 is the number of points
+    grid_x, grid_y = np.mgrid[-35:35:1000j, -35:35:1000j]
+    effective_area = []
+    for i in range(len(arm_angles)):
+        # generate the spiral arm medians
+        theta, rho = spiral_arm_medians(arm_angles[i], pitch_angles[i])
+        # generate the spiral arm points in spherical coordinates
+        rho_coords, theta_coords = generate_spiral_arm_points_spherical_coords(rho, theta, pitch_angles[i], transverse_distances)
+        # generate the spiral arm densities
+        density_spiral_arm = generate_spiral_arm_densities(rho, transverse_densities_initial)
+        # Convert to Cartesian coordinates
+        x = rho_coords*np.cos(theta_coords)
+        y = rho_coords*np.sin(theta_coords)
+        # calculate interpolated density for the spiral arm
+        interpolated_density = griddata((x, y), density_spiral_arm, (grid_x, grid_y), method='linear', fill_value=0)
+        # add the interpolated density to the total galactic density
+        effective_area = np.append(effective_area, np.sum(interpolated_density) * d_x * d_y)
+    return effective_area
+
+            
+
+plot_interpolated_galactic_densities()
+#plot_spiral_arms()
+#calc_modelled_emissivity()
+#plot_model_spiral_arm_densities()
--- test2.py
+++ test2.py
@@ -1,13 +1,70 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 
-# Create arrays of size (1000, 100) for x and y values
-n = 1000
-m = 100
+rho_min = 2.9           # kpc, minimum distance from galactic center to the beginning of the spiral arms.
+rho_max = 35 
+pitch_angles = np.radians(np.array([13.5, 13.5, 13.5, 15.5]))
+h = 2.5                 # kpc, scale length of the disk. The value Higdon and Lingenfelter used
+arm_angles = np.radians([70, 160, 250, 340])
 
-x_values = np.random.rand(n, 1)  # Replace with your actual x values
-y_values = np.random.rand(1, m)  # Replace with your actual y values
+def spiral_arm_medians(arm_angle, pitch_angle):
+    """
+    Args:
+        arm_angle: starting angle of the spiral arm, radians
+        pitch_angle: pitch angle of the spiral arm, radians
+    Returns:
+        values for thetas and the corresponding rhos for the spiral arm
+    """
+    
+    theta = [arm_angle]
+    rho = [rho_min]
+    dtheta = .01
+    k = np.tan(pitch_angle)
+    while rho[-1] < rho_max:
+        theta.append((theta[-1] + dtheta) )#% (2*np.pi)) # the % is to make sure that theta stays between 0 and 2pi
+        rho.append(rho_min*np.exp(k*(theta[-1] - theta[0])))
+    
+    print("Number of points for the given spiral arm: ", len(theta))
+    return np.array(theta), np.array(rho)
 
-# Combine x and y values into a single array of size (1000, 100, 2)
-combined_array = np.stack((x_values[:, :, np.newaxis], y_values[np.newaxis, :, :]), axis=2)
-print(combined_array.shape)
-# 'combined_array' is now a new array of size (1000, 100, 2)
+def arm_median_density(rho): 
+    """
+    Args:
+        rho: distance from the Galactic center
+        h: scale length of the disk
+    Returns:
+        density of the disk at a distance rho from the Galactic center
+    """
+    return np.exp(-rho/h)
+
+theta1, rho1 = spiral_arm_medians(arm_angles[0], pitch_angles[0])
+x1, y1 = rho1*np.cos(theta1), rho1*np.sin(theta1)
+theta2, rho2 = spiral_arm_medians(arm_angles[1], pitch_angles[1])
+x2, y2 = rho2*np.cos(theta2), rho2*np.sin(theta2)
+print(x2.shape, y2.shape)
+print(len(x2[len(x2)//2 : -1]))
+print(len(x2[0 : len(x2)//2]))
+x2 = np.concatenate((x2[(len(x2))//2 - 1 : -1], x2[0 : (len(x2) )//2]))
+y2 = np.concatenate((y2[(len(y2))//2 - 1 : -1], y2[0 : (len(y2) )//2]))
+
+#print(x2)
+#x2 = np.append(x2, x2[0 : len(x2)//2])
+print(x2.shape, y2.shape)
+
+densities1 = arm_median_density(rho1)
+densities2 = griddata((x1, y1), densities1, (x2, y2), method='linear', fill_value=np.nan)
+num_nan = np.count_nonzero(np.isnan(densities2))
+#print(densities2[len(densities2)-145:-1])
+print(num_nan)
+#print(len(densities2))
+plt.scatter(x1, y1, c=densities1, cmap='viridis', s=20, label='Spiral arm 1')
+plt.scatter(x2, y2, c=densities2,  cmap='viridis', s=20, label='Spiral arm 2')
+plt.legend()
+# Add a colorbar
+cbar = plt.colorbar()
+cbar.set_label('Density')
+
+# Set the aspect ratio to 'equal' for a more accurate representation of spherical coordinates
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
